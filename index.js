5650bea1cd63b6d12de6f3ae4f9421e0d092f9bd
Everton Ribeiro <nuxlli@gmail.com>
2013-07-31 15:18:51 -0300

    Página do azuki

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-  res.end('<center><h1>Port: ' + process.env.PORT + '</h1></center>\n');
+  //res.end('<center><h1>Port: ' + process.env.PORT + '</h1></center>\n');
+  res.end('<center><h1>Oi eu sou uma página do azuki!</h1></center>\n');
 }).listen(process.env.PORT);
 console.log('Server running');
