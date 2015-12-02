c132d425852b0d14cb0eb40ff401e14ce7c990e0
Gullit Miranda <gullitmiranda@gmail.com>
2015-12-02 17:25:58 -0200

    Adding basic Azkfile

---
+++ Azkfile.js
@@ -0,0 +1,39 @@
+/**
+ * Documentation: http://docs.azk.io/Azkfile.js
+ */
+// Adds the systems that shape your system
+systems({
+  azkdemo: {
+    // Dependent systems
+    depends: [],
+    // More images:  http://images.azk.io
+    image: {"docker": "azukiapp/node"},
+    // Steps to execute before running instances
+    provision: [
+      "npm install",
+    ],
+    workdir: "/azk/#{manifest.dir}",
+    shell: "/bin/bash",
+    command: ["npm", "start"],
+    wait: 20,
+    mounts: {
+      '/azk/#{manifest.dir}': sync("."),
+      '/azk/#{manifest.dir}/node_modules': persistent("./node_modules"),
+    },
+    scalable: {"default": 1},
+    http: {
+      domains: [ "#{system.name}.#{azk.default_domain}" ]
+    },
+    ports: {
+      // exports global variables
+      http: "3000/tcp",
+    },
+    envs: {
+      // Make sure that the PORT value is the same as the one
+      // in ports/http below, and that it's also the same
+      // if you're setting it in a .env file
+      NODE_ENV: "dev",
+      PORT: "3000",
+    },
+  },
+});
